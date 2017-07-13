8ad1ef6544180a9402ed0765ab3d04c36549992d
johnypark <g2njoy@ufl.edu>
2017-07-13 00:51:49 -0400

    071317

--- R/Yval.sps.R
+++ R/Yval.sps.R
@@ -1,24 +1,20 @@
-Yval.sps<-function(Data_Frame,sp_list, var1, var2)
+Yval.sps<-function(Data_Frame,sp_list)
 {
   ##Depends on sLeaves sBranch
   sp_list%<>% unique %>% as.character
   df.LIST<-list()
+  date.period=seq(16345,16702)  ##Date variable: need to make this more flexible
 
   for (j in 1:length(sp_list)){
 
-  fit.example<-Data_Frame %>%
+  pred.leaf<-Data_Frame %>%
     filter(spcode==sp_list[j]) %>%
     group_by(date) %>%
-    loess(sLeaves ~ as.numeric(date), data=., span=0.4)
+    loess(sLeaves ~ as.numeric(date), data=., span=0.4) %>% predict(.,data.frame(date = date.period), se = TRUE)
 
-  Y=predict(fit.example,data.frame(date = seq(16345, 16702)), se = TRUE) ##Date variable: need to make this more flexible
 
-  y.spcode=rep(sp_list[i],358)
-  y.date=as.Date(X,origin = "1970-01-01")
-  y.Leaf=Y$fit*100
-  k=length(sp_list)
-
-  df.LIST[[j]]<- data.frame("spcode"=sp_list,"date"=y.date,"Leaf_ratio"=y.Leaf)
+  y.date=as.Date(date.period,origin = "1970-01-01")
+  df.LIST[[j]]<- data.frame("spcode"=sp_list,"date"=y.date,"Mean_Leaves"=pred.Leaf)
 }
   ####
   do.call("rbind",df.LIST) %>%return
