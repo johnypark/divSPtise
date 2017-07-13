d3af8732289cc60bafb11fb5d0b140e991b2f576
johnypark <g2njoy@ufl.edu>
2017-07-13 10:37:40 -0400

    071417

--- R/Yval.sps.R
+++ R/Yval.sps.R
@@ -1,16 +1,18 @@
-Yval.sps<-function(Data_Frame,sp_list,span.d)
+Yval.sps<-function(Data_Frame,sp_list,loess.span)
 {
   ##Depends on sLeaves sBranch
   sp_list%<>% unique %>% as.character
   df.LIST<-list()
-  date.period=seq(16345,16702)  ##Date variable: need to make this more flexible
+  start.date<-cts[["date"]] %>% as.Date %>% unique %>% as.numeric %>% min
+  end.date<-cts[["date"]] %>% as.Date %>% unique %>% as.numeric %>% max
+  date.period=seq(start.date,end.date)  ##Date variable: need to make this more flexible
 
   for (j in 1:length(sp_list)){
 
   pred.leaf<-Data_Frame %>%
     filter(spcode==sp_list[j]) %>%
     group_by(date) %>%
-    loess(sLeaves ~ as.numeric(date), data=., span=span.d) %>% predict(.,data.frame(date = date.period), se = TRUE)
+    loess(sLeaves ~ as.numeric(date), data=., span=loess.span) %>% predict(.,data.frame(date = date.period), se = TRUE)
 
 
   y.date=as.Date(date.period,origin = "1970-01-01")
