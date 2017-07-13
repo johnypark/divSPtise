501a6e59893863cd2b6f63e774a455fcc434a391
johnypark <g2njoy@ufl.edu>
2017-07-13 02:04:35 -0400

    071317

--- R/Yval.sps.R
+++ R/Yval.sps.R
@@ -1,4 +1,4 @@
-Yval.sps<-function(Data_Frame,sp_list)
+Yval.sps<-function(Data_Frame,sp_list,span.d)
 {
   ##Depends on sLeaves sBranch
   sp_list%<>% unique %>% as.character
@@ -10,11 +10,11 @@ Yval.sps<-function(Data_Frame,sp_list)
   pred.leaf<-Data_Frame %>%
     filter(spcode==sp_list[j]) %>%
     group_by(date) %>%
-    loess(sLeaves ~ as.numeric(date), data=., span=0.4) %>% predict(.,data.frame(date = date.period), se = TRUE)
+    loess(sLeaves ~ as.numeric(date), data=., span=span.d) %>% predict(.,data.frame(date = date.period), se = TRUE)
 
 
   y.date=as.Date(date.period,origin = "1970-01-01")
-  df.LIST[[j]]<- data.frame("spcode"=sp_list[j],"date"=y.date,"Mean_Leaves"=setValueLimit(pred.leaf$fit,0,100),"Sd_Leaves"=pred.leaf$se.fit)
+  df.LIST[[j]]<- data.frame("spcode"=sp_list[j],"date"=y.date,"Loess_Leaves"=setValueLimit(pred.leaf$fit,0,100),"Sd_Leaves"=pred.leaf$se.fit)
 
 }
   ####
