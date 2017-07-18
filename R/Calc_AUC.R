d57057542a7890e1a6bf829762effa703008062a
johnypark <g2njoy@ufl.edu>
2017-07-18 10:45:20 -0400

    071817

--- R/Calc_AUC.R
+++ R/Calc_AUC.R
@@ -1,8 +1,8 @@
 #' function area under the curve
 #' @import dplyr
 #' @import magrittr
+#' @importFrom divsptise setValueLimit
 #' 071817
-#'
 #'
 #'
 #'
@@ -12,17 +12,17 @@
 
 
 Key.Name="tag"
-Data_Frame=cts
+Data_Frame=Dat_Fr
 
 
-Calc_AUC<-function(cts,Key.Name,Key.List,Foc.Var,Out.Var,date.min=NULL,date.max=NULL){
+Calc_AUC<-function(Dat_Fr,Key.Name,Key.List,Foc.Var,Out.Var,date.min=NULL,date.max=NULL){
   ##add Key.Name
   ##add Foc.Var
   ##add Out.Var =AUC_Rslt
 
 
-  MinD<-cts[["date"]]%>%as.numeric%>%min
-  MaxD<-cts[["date"]]%>%as.numeric%>%max
+  MinD<-Dat_Fr[["date"]]%>%as.numeric%>%min
+  MaxD<-Dat_Fr[["date"]]%>%as.numeric%>%max
 
   if(is.null(date.min)) {
     integral_x_min=0;
@@ -39,33 +39,21 @@ Calc_AUC<-function(cts,Key.Name,Key.List,Foc.Var,Out.Var,date.min=NULL,date.max=
     integral_x_max<-date.max%>%as.Date%>%as.numeric-MinD
   }
 
-  ##
-  ##
-  ##
-  ##
+
   AUC_Rslt <- data.frame(Key.List, NA)
   colnames(AUC_Rslt)=c(Key.Name,Out.Var)
-
-
-
-
-
-
-
-
 
   for(i in 1:length(Key.List)){
 
     date.vec=sprintf("as.numeric(%s)-min(as.numeric(%s))","date","date")
     setNames(date.vec,"date")
-    fit <- cts %>% group_by_(Key.Name) %>%
+    fit <- Dat_Fr %>% group_by_(Key.Name) %>%
       filter_("%s==%s"%>%sprintf(Key.Name,AUC_Rslt[[Key.Name]][i])) %>%
       arrange(date) %>%
       select_("date",Foc.Var) %>%
       mutate_(.dots = setNames(date.vec,"date")) %$%
       loess(.[[Foc.Var]]~.[["date"]])
-
-    try(int.fun <- function(x) predict(fit, newdata=x))
+    try(int.fun <- function(x) predict(fit, newdata=x)%>%setValueLimit(0,100)) #function setValueLimit: set upper and lower bound for prediction
     try(AUC_Rslt[[Out.Var]][i] <- integrate(int.fun, integral_x_min, integral_x_max)$value)
     try(rm(fit))
   }
